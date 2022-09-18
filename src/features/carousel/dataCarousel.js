250e1fb8215f909aafe6808f9ac85201054421f6
ProgrammerFront077 <alirezakh0090@gmail.com>
2022-09-18 12:28:34 +0430

    Initial commit

--- src/features/carousel/dataCarousel.js
+++ src/features/carousel/dataCarousel.js
@@ -1,11 +1,11 @@
 const dataCarousel = [
   {
-    url: 'https://images.pexels.com/photos/4551128/pexels-photo-4551128.jpeg?cs=srgb&dl=pexels-roman-odintsov-4551128.jpg&fm=jpg',
+    url: 'https://img.freepik.com/free-photo/chocolate-cookies-dark-white-chocolate-coffee-beans_8353-1497.jpg?w=996&t=st=1663486695~exp=1663487295~hmac=5699811d21bbc26227b4f666e02c31120f2b2414495f062bbd329e5874a89763',
     title: 'bean',
     text: 'A bean is the seed of one of several genera of the flowering plant family Fabaceae, which are used as vegetables for human or animal food',
   },
   {
-    url: 'https://images.pexels.com/photos/236804/pexels-photo-236804.jpeg?cs=srgb&dl=pexels-pixabay-236804.jpg&fm=jpg',
+    url: 'https://img.freepik.com/free-psd/delicious-raspberry-chocolate-cake-mockup_23-2149049419.jpg?w=996&t=st=1663486978~exp=1663487578~hmac=f0d298ab1330a738cb17c78bf087e0bebf85f4744781b97b00da7f2be977de18',
     title: 'cake',
     text: 'Cake is a flour confection made from flour, sugar, and other ingredients, and is usually baked',
   },
@@ -19,6 +19,11 @@ const dataCarousel = [
     url: 'https://images.pexels.com/photos/2074130/pexels-photo-2074130.jpeg?cs=srgb&dl=pexels-viktoria-alipatova-2074130.jpg&fm=jpg',
     title: 'coffe',
     text: 'Coffee is a major source of antioxidants in the diet. It has many health benefits, such as improved brain function and a lower risk of several diseases',
+  },
+  {
+    url: 'https://img.freepik.com/free-photo/close-up-shot-stone-slate-tray-with-sweet-natural-creamy-orange-ice-cream-set-decorated-with-fresh-mint-waffle-cones-dark-table-black-background-metal-scoop-is-laying-near_639032-565.jpg?w=996&t=st=1663487535~exp=1663488135~hmac=7f6761594aaf8b9e508f1263d64aaccac415dd02720506feb262c9b096c799b5',
+    title: 'ice cream',
+    text: 'Ice cream is a sweetened frozen food typically eaten as a snack or dessert. It may be made from milk or cream and is flavoured with a sweetener',
   },
 ];
 
